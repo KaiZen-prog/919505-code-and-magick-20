37d604ed0fc8c69a8c842830f3f6c25f2df95b62
Николай Карнаев <karnaev.nick@gmail.com>
2020-07-07 21:36:44 +0300

    Учебный проект: pimp my mage

---
+++ js/backend.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function () {
+  var URL_SEND = 'https://javascript.pages.academy/code-and-magick';
+  var URL_GET = 'https://javascript.pages.academy/code-and-magick/data';
+
+  var TIMEOUT_IN_MS = 10000;
+
+  var StatusCode = {
+    OK: 200
+  };
+
+  var dataExchangeHandler = function (xhr, onLoad, onError) {
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+  };
+
+  var errorHandler = function (xhr, onError) {
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+  };
+
+  window.save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    dataExchangeHandler(xhr, onLoad, onError);
+    errorHandler(xhr, onError);
+
+    xhr.open('POST', URL_SEND);
+    xhr.send(data);
+  };
+
+
+  window.load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    dataExchangeHandler(xhr, onSuccess, onError);
+    errorHandler(xhr, onError);
+
+    xhr.open('GET', URL_GET);
+    xhr.send();
+  };
+})();
--- js/utils.js
+++ js/utils.js
@@ -4,25 +4,55 @@
   var KEY_CODE_ENTER = 13;
   var KEY_CODE_ESCAPE = 27;
 
-  window.utils = {
-    getRandomArrayElement: function (array) {
+  var getRandomArrayElement = function (array) {
+    var rand = Math.floor(Math.random() * array.length);
+    return array[rand];
+  };
+
+  var getRandomArrayElementsCollection = function (array, newArrayLength) {
+    var newArray = [];
+
+    for (var i = 0; i < newArrayLength; i++) {
       var rand = Math.floor(Math.random() * array.length);
-      return array[rand];
-    },
 
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === KEY_CODE_ENTER) {
-        action();
-      }
-    },
-
-    onKeyDown: function (evt, action, userNameInput, setupSubmit) {
-      if ((evt.keyCode === KEY_CODE_ESCAPE) && (userNameInput !== document.activeElement)) {
-        evt.preventDefault();
-        action();
-      } else if ((evt.keyCode === KEY_CODE_ENTER) && (setupSubmit !== document.activeElement)) {
-        evt.preventDefault();
-      }
+      newArray.push(array[rand]);
+      array.splice(rand, 1);
     }
+    return newArray;
+  };
+
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === KEY_CODE_ENTER) {
+      action();
+    }
+  };
+
+  var onKeyDown = function (evt, action, userNameInput, setupSubmit) {
+    if ((evt.keyCode === KEY_CODE_ESCAPE) && (userNameInput !== document.activeElement)) {
+      evt.preventDefault();
+      action();
+    } else if ((evt.keyCode === KEY_CODE_ENTER) && (setupSubmit !== document.activeElement)) {
+      evt.preventDefault();
+    }
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.utils = {
+    getRandomArrayElement: getRandomArrayElement,
+    getRandomArrayElementsCollection: getRandomArrayElementsCollection,
+    isEnterEvent: isEnterEvent,
+    onKeyDown: onKeyDown,
+    errorHandler: errorHandler
   };
 })();
